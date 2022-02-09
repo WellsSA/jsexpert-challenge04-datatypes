6321420c923bfd16cf6d5a9d41747349e5a72f6f
WellsSA <wel.cavzod@gmail.com>
2022-02-09 09:42:17 -0300

    feat(CryptoService): main generator setup

---
+++ src/service/CryptoService.js
@@ -0,0 +1,13 @@
+import Crypto from '../entity/Crypto.js';
+import CryptoRepository from '../repository/CryptoRepository.js';
+
+class CryptoService {
+  constructor({ repository } = {}) {
+    this.repository = repository || new CryptoRepository();
+  }
+  async *list() {
+    // TODO: implementar generator que chama a repository fazendo a paginação
+  }
+}
+
+export default CryptoService;
